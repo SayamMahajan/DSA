8cffb3a132a844204b58f7a0ca64c041440010ac
SayamMahajan <mahajansayam3@gmail.com>
2024-06-22 13:13:00 +0530

    using hash approach

--- Array/FindingMissingElement.cpp
+++ Array/FindingMissingElement.cpp
@@ -18,11 +18,29 @@ void findElement(struct Array arr){
             }
         }
     }
+    cout<<endl;
 }
 
+//using hash approach
+void hashFind(struct Array arr){
+    Array *arr1;
+    arr1=new Array[11];
+    arr1->length=11;
+    arr1->size=11;
+    for (int i = 0; i <arr.length; i++){
+        arr1->A[arr.A[i]]=1;
+    }
+    for(int i=arr.A[0]; i<arr1->length;i++){
+        if(arr1->A[i]!=1){
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+}
 
 int main(){
     Array arr{{3,4,6,9,10},10,5};
     findElement(arr);
+    hashFind(arr);
     return 0;
 }
