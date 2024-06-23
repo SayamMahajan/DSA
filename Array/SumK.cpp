45e0c26a614f0a6104eb19b5b5709415e3f59c67
SayamMahajan <mahajansayam3@gmail.com>
2024-06-23 10:57:16 +0530

    for sorted array

--- Array/SumK.cpp
+++ Array/SumK.cpp
@@ -8,12 +8,31 @@ struct Array{
 };
 
 // O(n^2)
-void sumK(struct Array arr, int sum){
+void sumK(struct Array arr, int k){
     for (int i = 0; i < arr.length-1; i++){
         for (int j = i+1; j < arr.length; j++){
-            if(arr.A[i]+arr.A[j]==sum){
-                cout<<arr.A[i]<<"+"<<arr.A[j]<<" = "<<sum<<endl;
+            if(arr.A[i]+arr.A[j]==k){
+                cout<<arr.A[i]<<"+"<<arr.A[j]<<" = "<<k<<endl;
             }
+        }
+    }
+    cout<<endl;
+}
+
+//for sorted array
+void sortedSumK(struct Array arr, int k){
+    int i=0, j=arr.length-1;
+    while(i<j){
+        if(arr.A[i]+arr.A[j]==k){
+            cout<<arr.A[i]<<"+"<<arr.A[j]<<" = "<<k<<endl;
+            i++;
+            j--;
+        }
+        else if(arr.A[i]+arr.A[j]<k){
+            i++;
+        }
+        else{
+            j--;
         }
     }
     cout<<endl;
@@ -21,6 +40,8 @@ void sumK(struct Array arr, int sum){
 
 int main(){
     Array arr{{13,4,5,6,1,8,2,11,10,9},10,10};
+    Array arr1{{1,2,4,5,6,7,8,10,11,12},10,10};
     sumK(arr, 10);
+    sortedSumK(arr1, 10);
     return 0;
 }
