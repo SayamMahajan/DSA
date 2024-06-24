b2ff792511443d98df3c9f178dab9d95f93158c0
SayamMahajan <mahajansayam3@gmail.com>
2024-06-24 12:26:16 +0530

    length of string

---
+++ String/Length.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char s[]={'s','a','y','a','m','\0'};
+    int i;
+    for (i = 0; s[i]!='\0' ; i++){
+    }
+    cout<<"The lenght is "<<i<<endl;
+    return 0;
+}
