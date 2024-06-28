ecf5b637fcc0b7e65832ab557510c2973f3a2409
SayamMahajan <mahajansayam3@gmail.com>
2024-06-28 19:42:20 +0530

    adding sparse matrix

--- Matrix/SparseMatrix.cpp
+++ Matrix/SparseMatrix.cpp
@@ -45,11 +45,55 @@ class Sparse{
                 cout<<endl;
             }
         }
+    Sparse add(Sparse& s) {
+        if (m != s.m || n != s.n) {
+            cout << "Addition can't be performed" << endl;
+            return Sparse(0, 0, 0);
+        }
+
+        Sparse sum(m, n, num + s.num);
+
+        int i, j, k;
+        i = j = k = 0;
+
+        while (i < num && j < s.num) {
+            if (ele[i].i < s.ele[j].i) {
+                sum.ele[k++] = ele[i++];
+            }
+            else if (ele[i].i > s.ele[j].i) {
+                sum.ele[k++] = s.ele[j++];
+            }
+            else {
+                if (ele[i].j < s.ele[j].j) {
+                    sum.ele[k++] = ele[i++];
+                }
+                else if (ele[i].j > s.ele[j].j) {
+                    sum.ele[k++] = s.ele[j++];
+                }
+                else {
+                    sum.ele[k] = ele[i];
+                    sum.ele[k++].x = ele[i++].x + s.ele[j++].x;
+                }
+            }
+        }
+
+        for (; i < num; i++) {sum.ele[k++] = ele[i];}
+        for (; j < s.num; j++) {sum.ele[k++] = s.ele[j];}
+        sum.num = k;
+
+        return sum;
+    }
 };
 
 int main(){
     Sparse s1(5,5,5);
+    Sparse s2(5,5,7);
     s1.read();
     s1.display();
+    s2.read();
+    s2.display();
+    Sparse sum=s1.add(s2);
+    sum.display();
+
     return 0;
 }
